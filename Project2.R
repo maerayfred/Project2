ce28bf818a6febf81c162b8e81ad690b5b6a3d86
maerayfred <mfreder@ncsu.edu>
2024-11-03 10:57:26 -0500

    Numerical Summaries

--- Project2.R
+++ Project2.R
@@ -10,10 +10,10 @@ data2<-data|>
   mutate(User.Behavior.Class=as.factor(User.Behavior.Class))
  
 
-#One way contingency Table 
+#One way and Two way contingency Table 
 table(data2$Gender)
 table(data2$Operating.System)
-table(data2$gender,data2$Operating.System)
+
 
 data2|>
   drop_na(Gender,Operating.System)|>
@@ -31,6 +31,15 @@ pivot_wider(names_from = Gender,values_from = count)
 data2|>
   group_by(Gender)|>
   summarize(mean_age=mean(Age,na.rm=TRUE),med_age=median(Age,na.rm=TRUE),min_age=min(Age,na.rm=TRUE),Max_age=max(Age,na.rm = TRUE))
+
+data2|>
+  group_by(Gender)|>
+  summarize(mean_apps=mean(Number.of.Apps.Installed,na.rm=TRUE),sd_apps=sd(Number.of.Apps.Installed,na.rm=TRUE),med_apps=median(Number.of.Apps.Installed,na.rm=TRUE),iqr_apps=IQR(Number.of.Apps.Installed,na.rm=TRUE),min_apps=min(Number.of.Apps.Installed,na.rm=TRUE),Max_apps=max(Number.of.Apps.Installed,na.rm = TRUE))
+
+data2|>
+  group_by(Gender)|>
+  summarize(mean_scrntime=mean(Screen.On.Time..hours.day.,na.rm=TRUE),sd_scrntime=sd(Screen.On.Time..hours.day.,na.rm=TRUE),med_scrntime=median(Screen.On.Time..hours.day.,na.rm=TRUE),iqr_scrntime=IQR(Screen.On.Time..hours.day.,na.rm=TRUE),min_scrntime=min(Screen.On.Time..hours.day.,na.rm=TRUE),Max_scrntime=max(Screen.On.Time..hours.day.,na.rm = TRUE))
+
 
 
 
